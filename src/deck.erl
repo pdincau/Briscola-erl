1d91f5b7a1ea0ea55236b39529d38d9040bbd22a
Paolo D'Incau <paolo.dincau@gmail.com>
2011-08-22 11:20:17 +0200

    first commit

---
+++ src/deck.erl
@@ -0,0 +1,155 @@
+%%%-------------------------------------------------------------------
+%%% @author Paolo  <>
+%%% @copyright (C) 2011, Paolo 
+%%% @doc
+%%%
+%%% @end
+%%% Created : 16 Aug 2011 by Paolo  <>
+%%%-------------------------------------------------------------------
+-module(deck).
+
+-behaviour(gen_server).
+
+%% API
+-export([start_link/0]).
+
+%% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+	 terminate/2, code_change/3]).
+
+-define(SERVER, ?MODULE). 
+
+-include("../include/briscola.hrl").
+-record(state, {cards=[], briscola=nothing}).
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Starts the server
+%%
+%% @spec start_link() -> {ok, Pid} | ignore | {error, Error}
+%% @end
+%%--------------------------------------------------------------------
+start_link() ->
+    gen_server:start_link(?MODULE, [], []).
+
+%%%===================================================================
+%%% gen_server callbacks
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Initializes the server
+%%
+%% @spec init(Args) -> {ok, State} |
+%%                     {ok, State, Timeout} |
+%%                     ignore |
+%%                     {stop, Reason}
+%% @end
+%%--------------------------------------------------------------------
+init([]) ->
+    CreateCards = fun(Seed, Acc) ->
+			  [create_cards(Seed) | Acc]
+		  end,
+    Cards = lists:flatten(lists:foldl(CreateCards, [], [coppe, denari, spade, bastoni])),
+    [Briscola | ShuffledCards] = shuffle(Cards),
+    {ok, #state{cards = ShuffledCards, briscola = Briscola}}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling call messages
+%%
+%% @spec handle_call(Request, From, State) ->
+%%                                   {reply, Reply, State} |
+%%                                   {reply, Reply, State, Timeout} |
+%%                                   {noreply, State} |
+%%                                   {noreply, State, Timeout} |
+%%                                   {stop, Reason, Reply, State} |
+%%                                   {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_call(draw_card, _From, #state{cards = [Card | RestOfCards]} = State) ->
+    {reply, Card, State#state{cards = RestOfCards}};
+
+handle_call(draw_card, _From, #state{cards = [], briscola = Briscola} = State) ->
+    Reply = Briscola,
+    {reply, Reply, State};
+
+handle_call(_Request, _From, State) ->
+    Reply = ok,
+    {reply, Reply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling cast messages
+%%
+%% @spec handle_cast(Msg, State) -> {noreply, State} |
+%%                                  {noreply, State, Timeout} |
+%%                                  {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_cast(_Msg, State) ->
+    {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling all non call/cast messages
+%%
+%% @spec handle_info(Info, State) -> {noreply, State} |
+%%                                   {noreply, State, Timeout} |
+%%                                   {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called by a gen_server when it is about to
+%% terminate. It should be the opposite of Module:init/1 and do any
+%% necessary cleaning up. When it returns, the gen_server terminates
+%% with Reason. The return value is ignored.
+%%
+%% @spec terminate(Reason, State) -> void()
+%% @end
+%%--------------------------------------------------------------------
+terminate(_Reason, _State) ->
+    ok.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Convert process state when code is changed
+%%
+%% @spec code_change(OldVsn, State, Extra) -> {ok, NewState}
+%% @end
+%%--------------------------------------------------------------------
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
+shuffle(Cards) ->
+    shuffle(Cards, []).
+
+shuffle([], Acc) ->
+    Acc;
+
+shuffle(Cards, Acc) ->
+    {Leading, [H|T]} = lists:split(random:uniform(lists:flatlength(Cards)) -1, Cards),
+    shuffle(Leading ++ T, [H | Acc]).
+
+create_cards(Seed) ->
+    lists:foldl(fun(Number, Acc) ->
+			[#card{value=Number, seed=Seed} | Acc]
+		end, [], lists:seq(1, 10)).
+
